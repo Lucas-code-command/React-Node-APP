be4a3f699d551d8e0a194a7e680bf7f474521cba
Lucas Moliterno <lucas.moliterno1@gmail.com>
2023-04-13 21:45:03 -0300

    Update GetApi.js

--- src/App2/GetApi.js
+++ src/App2/GetApi.js
@@ -2,6 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+// ideia é criar um lugar de busca onde as pessoas possam ver o país onde querem morar
+// 1 passo é criar cards
+
 export default function GetApi(){
     const link = 'https://restcountries.com/v3.1/all'
 
@@ -29,6 +32,7 @@ export default function GetApi(){
 
     },[])
 
+
     const handleFilterChange = (e) => {
         setFilterQuery(e.target.value)
         
@@ -42,28 +46,17 @@ export default function GetApi(){
     }
 
 
-
-
-   
-
     const filteredData = data ? 
     data.filter((item) => 
         item.name.common.toLowerCase().includes(filterQuery.toLowerCase()) 
-        && (lessEqual === 'More than' ? (item.area > anotherFilter)
-            : (lessEqual === 'Less than') ? (item.area < anotherFilter)
-            : (lessEqual === 'Select a symbol') ? (item.area)
-            : (item.area).toString().startsWith(anotherFilter) ) //Equal than
-
-        //&& (item.capital).toLowerCase().includes(another.toString().toLowerCase())
+        && (lessEqual === 'More than' ? (item.area > anotherFilter): (lessEqual === 'Less than') ? (item.area < anotherFilter): (lessEqual === 'Select a symbol') ? (item.area): (item.area).toString().startsWith(anotherFilter) ) //Equal than
+        && Array.isArray(item.capital)
+        && (item.capital.some(city => city.toLowerCase().includes(another.toLowerCase())))
     ) 
     :[];
 
     
-
-    //console.log(data)
-    
-    
-
+    console.log(data)
 
     return(
         <div>   
@@ -83,9 +76,9 @@ export default function GetApi(){
                                 <input  class="form-control" id="name-input"required  type="text" value={filterQuery} onChange={handleFilterChange} placeholder='Name' />
                                 <label for="name-input">  Name</label>
                             </div>
-                            <div class="row">
-                                <div class='col'>
-                                    <select class='form-select' style={{ width: '100%' }} onChange={(e)=> setLessEqual(e.target.value)}>
+                            <div class='row row-2' >
+                                <div class='col' style={{ width: '100%'}}>
+                                    <select class='form-select'  onChange={(e)=> setLessEqual(e.target.value)}>
                                         <option>Select a symbol</option>
                                         <option> More than </option>
                                         <option> Less than </option>
@@ -102,8 +95,11 @@ export default function GetApi(){
                                 </div>
                             </div>
                            
-                            <div class='col'>
-                                <input style={{ width: '100%' }} disabled type="text" value={another} onChange={handleAnother} placeholder='Capital is not working because it is not a string' />
+                            <div class='col form-floating'>
+                                <input 
+                                class='form-control'
+                                style={{ width: '100%' }} type="capitals" id="capitals_filter"value={another} onChange={handleAnother} placeholder='Capitals' />
+                                <label for ='capitals_filter'> Capitals </label>
                             </div>
                         </form>
                     </div>
@@ -111,39 +107,34 @@ export default function GetApi(){
 
                 {loading && <h1>A moment, please....</h1>}
                 {error && <div>{`There is a problem fetching the post data - ${error}`}</div>}  
+                <div className='row'>
                 {data && filteredData.map((item, index) => (
-                   <div key={index}>
-                        <div class="row gx-2">
-                            <div class='col border' style={{color:'black'}}>
-                                <div>{item.name.common}</div>
-                            </div>
-                            <div class='col border' >
-                                <div>{item.flag}</div>
-                            </div>
-                            <div class='col border'>
-                                <div>{item.capital && item.capital.map((capitals)=>(
-                                    <li key={capitals}>{capitals}</li>
-                                ))}</div>
-                            </div>
-                            <div class='col border'>
-                                <div>{item.area}</div>
-                            </div>
-                            <div class='col border'>
-                                <ol>
-                                    {item.borders && item.borders.map((border) => (
-                                        <li key={border}>{border}</li>
-                                    ))}
-                                </ol>
-                            </div>
-
-                            
-                        </div>
-                   </div>
-                   
                     
+                        <div key={index} className='card col-md-3 m-3 p-0'>
+                            <div className="card-header">{item.name.common}</div>
+                            <div classname="card-body">
+                                <img src={item.flags.png} style={{width: '100%', display: 'block', margin: '0 auto'}}/>
+                                <ul>
+                                    <li className="list-group-item"><strong>Capital:</strong> {item.capital && item.capital.join(', ')}</li>
+                                    <li className='list-group-item'><strong>Area:</strong> {item.area}</li>
+                                    <li className='list-group-item'><strong>Continent:</strong> {item.continents && item.continents.map((continent)=>(<>{continent}</>))}</li>
+                                    <li className="list-group-item"><strong>Borders:</strong>
+                                        {item.borders && item.borders.map((border, index)=>(
+                                                <span key={`${border}-${index}`}> {border}, </span>
+                                             ))}
+                                     </li>
+                                     <li className='list-group-item'><strong>Language: </strong>
+                                        {item.languages && Object.entries(item.languages).map(([key,value])=>(
+                                            <>{value}, </>))}
+                                     </li>
+                                  </ul>
+                            </div>
+                        </div> 
                     ))}
             </div>
         </div>
+        </div>
     )
+   
 
 }
