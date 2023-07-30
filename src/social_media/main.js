48b8450af0e9e51f6cccf39820b63ccc716ae43f
Lucas Moliterno <lucas.moliterno1@gmail.com>
2023-07-30 20:30:43 -0300

    .

--- src/social_media/main.js
+++ src/social_media/main.js
@@ -1,6 +1,20 @@
-
+import {useState} from 'react'
+import Overlay_ from './Overlay'
 
 export default function Main(){
+
+    const [showOverlay, setShowOverlay]=useState(false)
+    const handOverlayOpen = () =>{
+        setShowOverlay(true)
+
+    }
+
+    const handOverlayClose = () =>{
+        setShowOverlay(false)
+    }
+
+
+
     return(
         <div className="container">
             <div class='row row-cols-2'> 
@@ -12,7 +26,7 @@ export default function Main(){
                     <p>@do usuário</p>
                     </div>
                     <div class='col' style={{backgroundColor:"green"}}>
-                        <h1>quem seguir?</h1>
+                        <h1>Como estou me sentindo?</h1>
                         <p>foto de perfil</p>
                         <h2>Nome</h2>
                         <h3>user</h3>
@@ -23,7 +37,10 @@ export default function Main(){
                 
                 <div class='row row-cols-1 row-cols-md-2'>
                         <div clss='col'>Hey</div>
+                        <button onClick={handOverlayOpen}>Open</button>
+                        {showOverlay  && <Overlay_ onClose={handOverlayClose}/>}
                         <div clss='col'>There</div>
+                        
                 </div>
             </div>
             
