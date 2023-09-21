825b1396529de99f4e8e1cde586752a4fd073a6b
Lucas Moliterno <lucas.moliterno1@gmail.com>
2023-09-21 11:34:33 -0300

    .

--- src/social_media/Main_about.js
+++ src/social_media/Main_about.js
@@ -1,11 +1,12 @@
 import { Col, Row } from "react-bootstrap";
+import CompanyLogo from 'src/social_media/img/CompanyLogo.png';
+
 
 
 export default function Main_about(){
     return(
         <div className="container">
-            <img src="src/social_media/img/COmpany-removebg-preview.png" alt='' />
-
+            <img src={CompanyLogo} alt='' />
         </div>
         
     )
